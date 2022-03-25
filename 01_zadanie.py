dac10e4c724be56292af83ff663b78a02d167729
mbak1994 <s.janczura@op.pl>
2022-03-25 19:03:39 +0100

    rozwiązanie

---
+++ 01_zadanie.py
@@ -0,0 +1,26 @@
+#Stwórz dwie zmienne `a` i `b` i przypisz do nich różne wartości.
+#Napisz program który zamienia wartości zmiennych miejscami.
+
+#sposób nr. 1
+# a = 10
+# b = 20
+#
+# print(a,b)
+# print(b,a)
+
+#sposób nr. 2
+#
+# a,b = 10,20
+# print(a,b)
+#
+# a,b = b,a
+# print(a,b)
+
+#sposób nr. 3 - bez przypisania liczb na początku
+
+# a = input("Wprowadź pierwszą liczbę: ")
+# b = input("Wprowadź drugą liczbę: ")
+# print(a,b)
+# print(b,a)
+
+
